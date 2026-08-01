12253af88a74a3a0f4081a765bbc80725313ddbe
Joshua Thome <joshpthome@gmail.com>
2026-08-01 14:54:14 -0500

    Add project files.

---
+++ OuterWildsModJam6.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using HarmonyLib;
+using OWML.Common;
+using OWML.ModHelper;
+
+namespace OuterWildsModJam6
+{
+    public class OuterWildsModJam6 : ModBehaviour
+    {
+        public static OuterWildsModJam6 Instance;
+        public INewHorizons NewHorizons;
+
+        public void Awake()
+        {
+            Instance = this;
+        }
+
+        public void Start()
+        {
+            NewHorizons = ModHelper.Interaction.TryGetModApi<INewHorizons>("xen.NewHorizons");
+            NewHorizons.LoadConfigs(this);
+
+            new Harmony(ModHelper.Manifest.UniqueName).PatchAll(Assembly.GetExecutingAssembly());
+
+            OnCompleteSceneLoad(OWScene.TitleScreen, OWScene.TitleScreen);
+            LoadManager.OnCompleteSceneLoad += OnCompleteSceneLoad;
+        }
+
+        public void OnCompleteSceneLoad(OWScene previousScene, OWScene newScene)
+        {
+            if (newScene != OWScene.SolarSystem) return;
+            ModHelper.Console.WriteLine("Loaded into solar system!", MessageType.Success);
+        }
+    }
+
+}
